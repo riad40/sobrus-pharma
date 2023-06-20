b49c7ca81044e4ce19bf40598c3d6c69f2b3ba97
riad40 <riadabdelaziz40@gmail.com>
2023-06-20 11:14:35 +0100

    added Product type

---
+++ src/@types/index.ts
@@ -0,0 +1,11 @@
+type Product = {
+    id: number
+    name: string
+    dci: string
+    classTherapeutic: string
+    laboratory: string
+    avatar: string
+    barcode: string
+}
+
+export type { Product }
