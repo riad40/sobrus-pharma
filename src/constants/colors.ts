9d6e7d687f1f36b0c22392556bc1bb2de947c026
riad40 <riadabdelaziz40@gmail.com>
2023-06-21 15:39:28 +0100

    added project constants

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,7 +1,8 @@
 const colors = {
     primary: '#00C1B3',
     primary_disabled: '#00FFF0',
-    secondary: '#A6A6A6'
+    secondary: '#A6A6A6',
+    white: '#FFFFFF'
 }
 
 export default colors
---
+++ src/constants/dimensions.ts
@@ -0,0 +1,8 @@
+import { Dimensions } from 'react-native'
+
+const { width, height } = Dimensions.get('window')
+
+const SCREEN_WIDTH = width
+const SCREEN_HEIGHT = height
+
+export { SCREEN_WIDTH, SCREEN_HEIGHT }
