89b30b5af4fb14409ec0cfba4d4638dddfacd101
riad40 <riadabdelaziz40@gmail.com>
2023-06-22 11:14:32 +0100

    created redux store

---
+++ src/state/store.ts
@@ -0,0 +1,29 @@
+import { configureStore, combineReducers, getDefaultMiddleware } from '@reduxjs/toolkit'
+import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux'
+import inventorySlice from './features/InventorySlice'
+
+// combine all reducers
+const rootReducer = combineReducers({
+    inventories: inventorySlice
+})
+
+// create store
+const store = configureStore({
+    reducer: rootReducer,
+    middleware: getDefaultMiddleware({
+        serializableCheck: false,
+        immutableCheck: false
+    })
+})
+
+// define @types
+export type RootState = ReturnType<typeof rootReducer>
+
+export type AppDispatch = typeof store.dispatch
+
+export const useAppDispatch = () => useDispatch<AppDispatch>()
+
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+
+// export store
+export default store
